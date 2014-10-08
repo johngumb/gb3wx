9266d23d9fcfe29f46abd6ac23efc7f896636375
John Gumb <john@gumb.org>
2014-10-08 01:29:47 +0100

    some basic stuff working around serial port control

--- gb3wx-record.py
+++ gb3wx-record.py
@@ -3,19 +3,23 @@ from serial import Serial
 from fcntl import  ioctl
 from termios import (
     TIOCMIWAIT,
-    TIOCM_RNG,
     TIOCM_DSR,
-    TIOCM_CD,
     TIOCM_CTS
 )
 
-g_wait_signals = (TIOCM_DSR |
-                TIOCM_CTS)
+global g_wait_signals
+g_wait_signals = (TIOCM_DSR | TIOCM_CTS)
 
-def wait_for_qso_start():
+def wait_for_qso_start(ser):
+    print "wait for qso start"
+
     global g_wait_signals
 
-    ioctl(ser.fd, TIOCMIWAIT, wait_signals)
+    ioctl(ser.fd, TIOCMIWAIT, g_wait_signals)
+    print "DSR",ser.getDSR()
+    print "CTS",ser.getCTS()    
+    print "QSO started"
+
     dsr = ser.getDSR()
     dtr = ser.getCTS()
 
@@ -27,17 +31,27 @@ def wait_for_qso_start():
     if dtr:
         result = "6_10"
 
+    # need to return TIOCM_DSR or TIOCM_CTS here
+    # so we can wait for the appropriate one to
+    # go away.
     return result
 
-def wait_for_inactivity():
-    wait_for_qso_stop()
+def wait_for_inactivity(ser):
+    dsr = ser.getDSR()
+    dtr = ser.getCTS()
 
-def wait_for_qso_stop():
+    if not dsr and not dtr:
+        return
+
+    wait_for_qso_stop(ser)
+
+def wait_for_qso_stop(ser):
+    print "wait for qso stop"
     global g_wait_signals
-    ioctl(ser.fd, TIOCMIWAIT, wait_signals)
-    ser.getDSR()
-    ser.getCTS()
-
+    ioctl(ser.fd, TIOCMIWAIT, g_wait_signals)
+    print "DSR",ser.getDSR()
+    print "CTS",ser.getCTS()    
+    print "QSO stopped"
     return
 
 def main():
@@ -54,11 +68,13 @@ def main():
 
         mode = wait_for_qso_start(ser)
 
-        rec_handle = start_thread(start_record, mode)
+#        rec_handle = start_thread(start_record, mode)
 
+        print "recording"
         wait_for_qso_stop(ser)
-
-        stop_record(rec_handle)
+        print "stopping recording"
+        
+#        stop_record(rec_handle)
 
 
 if __name__ == "__main__":
